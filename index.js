3138e9b01c8b7170ef897ff74b25353d26fec590
Bryan McAfee <brymcafee@gmail.com>
2017-09-15 17:49:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ if (name === "Joe") {
 } else {
   console.log("Expexted the name to be Susan")
 }
+if (height === 70) {
+  console.log("The height is correct")
+} else {
+  console.log("Expected the height to be 70")
+}
 
 
 // Don't worry about this
